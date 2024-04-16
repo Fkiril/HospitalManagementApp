72e21dea4bda8aaea1e7d8ae6f39ff2868ac2ccf
Duy Thuc <thuc.nguyenkhmtk22@hcmut.edu.vn>
2024-04-16 22:35:51 +0700

    complete view of calendar

--- HospitalManagementApp/wwwroot/js/calendar.js
+++ HospitalManagementApp/wwwroot/js/calendar.js
@@ -1,4 +1,5 @@
-﻿var staffData = document.getElementById("cal").innerText;
+﻿var date = document.getElementById("date").innerText;
+var shift = document.getElementById("shift").innerText;
 
 const a2 = document.getElementById('a2');
 const a3 = document.getElementById('a3');
@@ -25,57 +26,55 @@ const c6 = document.getElementById('c6');
 const c7 = document.getElementById('c7');
 const c8 = document.getElementById('c8');
 
+const day1 = document.getElementById('day1');
+const day2 = document.getElementById('day2');
+const day3 = document.getElementById('day3');
+const day4 = document.getElementById('day4');
+const day5 = document.getElementById('day5');
+const day6 = document.getElementById('day6');
+const day7 = document.getElementById('day7');
+
 function morning(element1, element2, element3) {
     element1.innerText = "Work";
     element2.innerText = "";
     element3.innerText = "";
 }
 function afternoon(element1, element2, element3) {
-    element1.innerText = "Work";
-    element2.innerText = "";
+    element1.innerText = "";
+    element2.innerText = "Work";
     element3.innerText = "";
 }
 function evening(element1, element2, element3) {
-    element1.innerText = "Work";
+    element1.innerText = "";
     element2.innerText = "";
-    element3.innerText = "";
+    element3.innerText = "Work";
 }
 
 function day(text, element1, element2, element3) {
-    if (text == "morning") morning(element1, element2, element3);
-    else if (text == "afternoon") afternoon(element1, element2, element3);
+    if (text == "Morning") morning(element1, element2, element3);
+    else if (text == "Afternoon") afternoon(element1, element2, element3);
     else evening(element1, element2, element3);
 }
 
 //xu li
-var lines = staffData.split(' ');
-for (var i = 0; i < lines.length; i+=2) {
-    var p1 = lines[i];
-    var p2 = lines[i + 1];
+var lines = shift.split(", ");
 
-    switch (p1) {
-        case "Monday":
-            day(p2, a2, b2, c2);
-            break;
-        case "Tuesday":
-            day(p2, a3, b3, c3);
-            break;
-        case "Wednesday":
-            day(p2, a4, b4, c4);
-            break;
-        case "Thursday":
-            day(p2, a5, b5, c5);
-            break;
-        case "Friday":
-            day(p2, a6, b6, c6);
-            break;
-        case "Saturday":
-            day(p2, a7, b7, c7);
-            break;
-        case "Sunday":
-            day(p2, a8, b8, c8);
-            break;
-        default:
-            break;
-    }
-}
+//update for calendar
+day(lines[0], a2, b2, c2);
+day(lines[1], a3, b3, c3);
+day(lines[2], a4, b4, c4);
+day(lines[3], a5, b5, c5);
+day(lines[4], a6, b6, c6);
+day(lines[5], a7, b7, c7);
+day(lines[6], a8, b8, c8);
+
+
+var dates = date.split(", ");
+
+day1.innerText = dates[0];
+day2.innerText = dates[1];
+day3.innerText = dates[2];
+day4.innerText = dates[3];
+day5.innerText = dates[4];
+day6.innerText = dates[5];
+day7.innerText = dates[6];
